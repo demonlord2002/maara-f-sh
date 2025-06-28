f6b4bbf5b0dedb832455e9e04a3c73a53661b0ba
cookies2002 <cookies2002boy@gmail.com>
2025-06-29 02:23:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv()
 API_ID = 12345678  # Your API ID from my.telegram.org
 API_HASH = "your_api_hash"  # Your API HASH
 BOT_TOKEN = "your_bot_token"  # From BotFather
-OWNER_IDS = [123456789, 7049074888]  # Add your Telegram user ID and your friend's ID
+OWNER_IDS = [123456789, 098766665]  # Add your Telegram user ID and your friend's ID
