00e687b2833954431a1463a1ad458a8e0c22a0b4
cookies2002 <cookies2002boy@gmail.com>
2025-06-28 16:36:40 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TG_BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
+APP_ID = int(os.getenv("APP_ID"))
+API_HASH = os.getenv("API_HASH")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
