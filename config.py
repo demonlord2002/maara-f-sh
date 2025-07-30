dd4b43764685f2f40bbb2f58c8b801fcb022886c
cookies2002 <cookies2002boy@gmail.com>
2025-07-30 20:22:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
+load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
+
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
