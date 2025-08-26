8a75ba3cb9074b453edcedd01d6c2ca2a29c8067
demonlord2002 <drdoom2003p@gmail.com>
2025-08-27 02:02:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,25 @@
+import os
 import re
-from os import environ
 
 # ---------------- TELEGRAM BOT CONFIG ----------------
-API_ID = int(environ.get("API_ID", 22201946))  
-API_HASH = environ.get("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7535712545:AAHFTW9Adh530PCqTXbqzsgWiQpL7ZUTrWM")
+API_ID = int(os.getenv("API_ID", 22201946))
+API_HASH = os.getenv("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7535712545:AAHFTW9Adh530PCqTXbqsgWiQpL7ZUTrWM")
 
 # ---------------- CHANNELS & OWNER ----------------
-DATABASE_CHANNEL = environ.get("DATABASE_CHANNEL", "@madara_db_test")
-FORCE_SUBSCRIBE_CHANNEL = environ.get("FORCE_SUBSCRIBE_CHANNEL", "@Fallen_Angels_Team")
+DATABASE_CHANNEL = os.getenv("DATABASE_CHANNEL", "@madara_db_test")
+FORCE_SUBSCRIBE_CHANNEL = os.getenv("FORCE_SUBSCRIBE_CHANNEL", "@Fallen_Angels_Team")
 
-OWNER_IDS = [int(x) for x in environ.get("OWNER_IDS", "7813285237").split()]
-OWNER_USERNAME = environ.get("OWNER_USERNAME", "SunsetOfMe")
-SUPPORT_LINK = environ.get("SUPPORT_LINK", "https://t.me/Fallen_Angels_Team")
+# OWNER_IDS supports multiple comma-separated IDs
+OWNER_IDS = [int(x.strip()) for x in os.getenv("OWNER_IDS", "7813285237").split(",") if x.strip()]
+OWNER_USERNAME = os.getenv("OWNER_USERNAME", "SunsetOfMe")
+SUPPORT_LINK = os.getenv("SUPPORT_LINK", "https://t.me/Fallen_Angels_Team")
 
 # ---------------- MONGO DB ----------------
-MONGO_URI = environ.get("MONGO_URI", "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+)
+
+# ---------------- OPTIONAL ----------------
+WEB_URL = os.getenv("WEB_URL", None)  # Optional external URL for permanent links
