e8a69bc950a6cd9125a13005d261abef79a0ac48
demonlord2002 <drdoom2003p@gmail.com>
2025-08-27 01:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
+import re
+from os import environ
+
 # ---------------- TELEGRAM BOT CONFIG ----------------
-API_ID = 22201946                    # Your Telegram API_ID
-API_HASH = "f4e7f0de47a09671133ecafa6920ebbe"     # Your Telegram API_HASH
-BOT_TOKEN = "7535712545:AAHFTW9Adh530PCqTXbqzsgWiQpL7ZUTrWM"   # Bot token from @BotFather
+API_ID = int(environ.get("API_ID", 22201946))  
+API_HASH = environ.get("API_HASH", "f4e7f0de47a09671133ecafa6920ebbe")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7535712545:AAHFTW9Adh530PCqTXbqzsgWiQpL7ZUTrWM")
 
 # ---------------- CHANNELS & OWNER ----------------
-DATABASE_CHANNEL = "@madara_db_test"   # Channel where files are stored
-FORCE_SUBSCRIBE_CHANNEL = "@Fallen_Angels_Team"  # Channel users must join
-OWNER_IDS = [7813285237]                     # Your Telegram ID(s) (numeric)
-OWNER_USERNAME = "SunsetOfMe"               # Your Telegram username
-SUPPORT_LINK = "https://t.me/Fallen_Angels_Team"  # Support channel link
+DATABASE_CHANNEL = environ.get("DATABASE_CHANNEL", "@madara_db_test")
+FORCE_SUBSCRIBE_CHANNEL = environ.get("FORCE_SUBSCRIBE_CHANNEL", "@Fallen_Angels_Team")
+
+OWNER_IDS = [int(x) for x in environ.get("OWNER_IDS", "7813285237").split()]
+OWNER_USERNAME = environ.get("OWNER_USERNAME", "SunsetOfMe")
+SUPPORT_LINK = environ.get("SUPPORT_LINK", "https://t.me/Fallen_Angels_Team")
 
 # ---------------- MONGO DB ----------------
-MONGO_URI = "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = environ.get("MONGO_URI", "mongodb+srv://rubesh08virat:rubesh08virat@cluster0.d33p1rm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
